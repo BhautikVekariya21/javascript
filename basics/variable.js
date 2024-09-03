9a4a68310a2a111fca08b0e29b79b7f2427f0bd2
Bhautik Vekariya <128315385+BhautikVekariya21@users.noreply.github.com>
2024-09-03 17:23:01 +0000

    container

---
+++ basics/variable.js
@@ -0,0 +1,10 @@
+const num = 23;
+let str = "engine";
+var pass = 11215;
+city = "Halstatt";
+
+console.log(num);
+console.log(str);
+console.log(pass);
+console.log(city);
+console.table([num,str,pass,city]);
