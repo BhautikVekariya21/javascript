f79d8b8553bc7ed0d940e92e98857a0f6a6da117
Bhautik Vekariya <128315385+BhautikVekariya21@users.noreply.github.com>
2024-09-05 18:09:56 +0000

    global scope  & more

---
+++ 02basics/scopes.js
@@ -0,0 +1,37 @@
+let asw = 577;
+if(true){
+    // let asw = 212;
+    let qwe = 89;
+    console.log(asw);
+}
+console.log(asw);
+// console.log(qwe);
+
+function outer (){
+    const name="vishakha"
+    function inner(){
+        const namee="vishakhapattanam"
+        console.log(namee)
+        console.log(name)
+        console.log(namee + name)
+
+    }
+    inner()
+    // console.log(namee)
+    console.log(name)
+}
+outer();
+
+console.log(finland("heilsinki"));
+
+
+function finland(city){
+    return city
+}
+
+console.log(country);
+
+const country =function norway(city){
+    return city
+}
+console.log(norway("oslo"));
