19f753c8e2db9b0e0bafd6aeebe6d9ddbb586f88
Bhautik Vekariya <128315385+BhautikVekariya21@users.noreply.github.com>
2024-09-05 07:55:57 +0000

    container

---
+++ basics/summary.js
@@ -0,0 +1,46 @@
+const score = 98;
+const science = 97.89;
+const exam = true;
+const what = null;
+const email = "heilsinki2gamial.com";
+const vitenary = Symbol('96564');
+const visinary = Symbol('96564');
+
+console.log(score);
+console.log(typeof score);
+
+console.log(science);
+console.log(typeof science);
+
+console.log(exam);
+console.log(typeof exam);
+
+console.log(what);
+console.log(typeof what);
+
+console.log(email);
+console.log(typeof email);
+
+console.log(vitenary);
+console.log(typeof vitenary);
+
+console.log(visinary);
+console.log(typeof visinary);
+
+console.log(visinary==vitenary);
+console.log(visinary===vitenary);
+
+const fruit = ["apple","berries","watermelon","guava","pineapple","strawberry","custardapple"]
+console.log(fruit);
+console.log(typeof fruit);
+console.log(Array.isArray(fruit));
+ 
+
+let vegetable = {
+    name :"carrot",
+    season:"winter",
+    price:"90",
+    vitamin:"A",
+}
+console.log(vegetable);
+console.log(typeof vegetable);
