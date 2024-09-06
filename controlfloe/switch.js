236c28e19536dbb568542e4da69039c7ce48dbfd
Bhautik Vekariya <128315385+BhautikVekariya21@users.noreply.github.com>
2024-09-06 02:19:08 +0000

    switch case

---
+++ controlfloe/switch.js
@@ -0,0 +1,50 @@
+
+const fruit = "custard apple"
+
+switch (fruit) {
+    case "Apples":
+        console.log(`value of fruit is ${fruit}`);
+        break;
+    case "Strawberries":
+        console.log(`value of fruit is ${fruit}`);
+        break;
+    case "Bananas":
+        console.log(`value of fruit is ${fruit}`);
+        break;
+    case "Peaches":
+        console.log(`value of fruit is ${fruit}`);
+        break;
+    case "Mangoes":
+        console.log(`value of fruit is ${fruit}`);
+        break;
+    case "Raspberry":
+        console.log(`value of fruit is ${fruit}`);
+        break;
+    case "Red Apple":
+        console.log(`value of fruit is ${fruit}`);
+        break;
+    case "Red Cherry":
+        console.log(`value of fruit is ${fruit}`);
+        break;
+    case "Cranberry ":
+        console.log(`value of fruit is ${fruit}`);
+        break;
+    case "Redcurrant":
+        console.log(`value of fruit is ${fruit}`);
+        break;
+    case "Red Grape":
+        console.log(`value of fruit is ${fruit}`);
+        break;
+    case "Green Grape":
+        console.log(`value of fruit is ${fruit}`);
+        break;
+    case "Common Pear":
+        console.log(`value of fruit is ${fruit}`);
+        break;
+    case "custard apple":
+        console.log(`value of fruit is ${fruit}`);
+        break;
+    default:
+        console.log(`value of fruit is ${fruit}`);
+        break;
+}
