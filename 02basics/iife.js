24d93a1c2dabe60485f38682283000516fd1ef9a
Bhautik Vekariya <128315385+BhautikVekariya21@users.noreply.github.com>
2024-09-06 00:29:31 +0000

    reduce global pollute

---
+++ 02basics/iife.js
@@ -0,0 +1,25 @@
+const counter = (function () {
+    let count = 0; 
+
+    return {
+        increment: function () {
+            count++;
+            console.log("Counter:", count);
+        },
+        getValue: function () {
+            return count;
+        }
+    };
+})();
+
+counter.increment(); 
+counter.increment(); 
+console.log(counter.getValue());
+
+((name)=>{
+    console.log(`welcome ${name}`)
+})("Brinjal");
+
+(function vinuska(){
+    console.log(`hello vinuska`)
+})();
