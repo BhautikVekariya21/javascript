84bb623fd6489225c9d6cecf91141fdd6891e44d
Bhautik Vekariya <128315385+BhautikVekariya21@users.noreply.github.com>
2024-10-05 19:07:07 +0530

    prototype
    Explanation of Key Concepts
    Constructor Function:
    
    The Country function is a constructor that creates country objects with specific properties and methods.
    Prototype Inheritance:
    
    The __proto__ property is used to establish a prototype chain, allowing properties and methods from one object to be accessible in another object.
    Object and Array Prototypes:
    
    New methods (fin for objects and fini for arrays) are added to the prototypes, which means all objects and arrays can access these methods.
    Logging Prototype Chain:
    
    The console.log statements demonstrate how properties can be accessed from different levels of the prototype chain.
    String Prototype Method:
    
    The trueLength method is added to the String prototype to calculate the length of the string after trimming whitespace. This shows how we can extend built-in types in JavaScript.
    Output
    When you run this code, the output will reflect the statements in the console.log functions, indicating property values from the objects and demonstrating the prototype chain's functionality. If you uncomment the line with euCountryNames.fin(), it will throw an error because that method is not directly available to the euCountryNames array.

--- oop/prototype.js
+++ oop/prototype.js
@@ -1,3 +1,4 @@
+// Array of EU country names
 // const euCountryNames = [
 //     "Germany",
 //     "France",
@@ -6,73 +7,88 @@
 //     "Sweden"
 // ];
 
-// function Country(countryname, area, population, capital) {
-//     this.countryname = countryname;
-//     this.area = area;
-//     this.population = population;
-//     this.capital = capital;
-//     this.greeting = function () {
-//         console.log(`Welcome to ${this.countryname}, which has the capital ${this.capital}. It covers an area of ${this.area} km² and has a population of ${this.population}.`);
-//     };
-// }
+// Constructor function for creating country objects
+function Country(countryname, area, population, capital) {
+    this.countryname = countryname; // Name of the country
+    this.area = area; // Area of the country in km²
+    this.population = population; // Population of the country
+    this.capital = capital; // Capital city of the country
+    // Method to greet and provide information about the country
+    this.greeting = function () {
+        console.log(`Welcome to ${this.countryname}, which has the capital ${this.capital}. It covers an area of ${this.area} km² and has a population of ${this.population}.`);
+    };
+}
 
-// Object.prototype.fin = function () {
-//     console.log('fin is present in all objects');
-// };
+// Adding a method to the Object prototype, available to all objects
+Object.prototype.fin = function () {
+    console.log('fin is present in all objects');
+};
 
-// Array.prototype.fini = function () {
-//     console.log('fini is present in all arrays');
-// };
+// Adding a method to the Array prototype, available to all arrays
+Array.prototype.fini = function () {
+    console.log('fini is present in all arrays');
+};
 
-// const germany = new Country("Germany", 357022, 83166711, "Berlin");
-// const france = new Country("France", 551695, 67372000, "Paris");
+// Creating instances of the Country constructor
+const germany = new Country("Germany", 357022, 83166711, "Berlin");
+const france = new Country("France", 551695, 67372000, "Paris");
 
-// germany.fin(); 
-// euCountryNames.fin(); 
-// euCountryNames.fini(); 
+// Invoking the fin method on the germany object (object instance)
+germany.fin(); 
+// Invoking the fin method on the euCountryNames array (array instance)
+// euCountryNames.fin(); // This will throw an error if uncommented, as euCountryNames is not an object.
+// Invoking the fini method on the euCountryNames array (array instance)
+euCountryNames.fini(); 
 
-// germany.greeting(); 
-// france.greeting();
+// Calling the greeting method for both Germany and France
+germany.greeting(); 
+france.greeting();
 
+// Creating objects representing a village, district, state, country, and continent
 const Village = {
-    name: "Riverdale",
-    population: 1500
+    name: "Riverdale", // Name of the village
+    population: 1500 // Population of the village
 };
 
 const District = {
-    name: "River District",
-    numVillages: 20
+    name: "River District", // Name of the district
+    numVillages: 20 // Number of villages in the district
 };
 
 const State = {
-    name: "Sunny State",
-    numDistricts: 5
+    name: "Sunny State", // Name of the state
+    numDistricts: 5 // Number of districts in the state
 };
 
 const Country = {
-    name: "Dreamland",
-    numStates: 10
+    name: "Dreamland", // Name of the country
+    numStates: 10 // Number of states in the country
 };
 
 const Continent = {
-    name: "Fictional Continent",
-    numCountries: 50
+    name: "Fictional Continent", // Name of the continent
+    numCountries: 50 // Number of countries in the continent
 };
 
-District.__proto__ = Village;
-State.__proto__ = District;
-Country.__proto__ = State;
-Continent.__proto__ = Country;
+// Setting up prototype inheritance
+District.__proto__ = Village; // District inherits from Village
+State.__proto__ = District; // State inherits from District
+Country.__proto__ = State; // Country inherits from State
+Continent.__proto__ = Country; // Continent inherits from Country
 
+// This alternative way of setting prototype also works
 Object.setPrototypeOf(Country, State);
 Object.setPrototypeOf(State, District);
 
-console.log(Country.name); 
-console.log(Country.numDistricts); 
-console.log(Continent.population); 
+// Logging properties from different levels of the prototype chain
+console.log(Country.name); // Outputs: Dreamland (from Country object)
+console.log(Country.numDistricts); // Outputs: 5 (inherited from State)
+console.log(Continent.population); // Outputs: undefined (as Continent has no population property)
 
+// Adding a method to the String prototype to calculate the true length of a string
 String.prototype.trueLength = function(){
     console.log(`True length of '${this.trim()}': ${this.trim().length}`);
 };
 
-Continent.name.trueLength(); 
+// Calling the trueLength method on the name property of the Continent object
+Continent.name.trueLength(); // Outputs the length of 'Fictional Continent'
