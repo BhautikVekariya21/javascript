94a501873bb3d474bc24c6c05c19806933b102a8
Bhautik Vekariya <128315385+BhautikVekariya21@users.noreply.github.com>
2024-10-05 19:04:43 +0530

    oop
    Explanation of Key Concepts
    Object Definition:
    
    elonMusk: An object representing Elon Musk, with properties for his name, gender, children, and state, as well as a method getInfo to display this information.
    Constructor Function:
    
    country: A constructor function to create country objects with properties for the country's name, area, population, and capital, as well as a method greeting to provide a welcoming message about the country.
    Instance Creation:
    
    Instances of the country constructor are created using the new keyword, which allows you to create distinct objects (e.g., monaco, finland, liechtenstein) with the specified properties.
    Method Invocation:
    
    The getInfo method of the elonMusk object is called to display information about him.
    The greeting method is called on each country instance to output relevant information about each country.

--- oop/oop.js
+++ oop/oop.js
@@ -1,37 +1,62 @@
+// Creating an object representing Elon Musk
 const elonMusk = {
-    name: "Elon Musk",
-    gender: "Male",
-    children: ["Nevada", "Griffin", "X Æ A-Xii", "Exa Dark Sideræl"],
-    state: "Texas",
+    name: "Elon Musk", // Name property
+    gender: "Male", // Gender property
+    children: ["Nevada", "Griffin", "X Æ A-Xii", "Exa Dark Sideræl"], // Array of children names
+    state: "Texas", // State property
+    // Method to log information about Elon Musk
     getInfo: function() {
-        console.log(`Name: ${this.name}`);
-        console.log(`Gender: ${this.gender}`);
-        console.log(`Children: ${this.children.join(", ")}`);
-        console.log(`State: ${this.state}`);
-        console.log(this)
+        console.log(`Name: ${this.name}`); // Logs the name
+        console.log(`Gender: ${this.gender}`); // Logs the gender
+        console.log(`Children: ${this.children.join(", ")}`); // Logs the children as a comma-separated string
+        console.log(`State: ${this.state}`); // Logs the state
+        console.log(this); // Logs the entire object
     }
 };
 
-elonMusk.getInfo();
+// Call the getInfo method to log Elon Musk's information
+elonMusk.getInfo(); // This will output the information about Elon Musk
 
-function country(countryname,area,population,capital) {
-    this.area=area;
-    this.countryname=countryname;
-    this.population=population;
-    this.capital=capital,
-    this.greeting = function (){
-        console.log(`welcome to the ${countryname}and has a capital ${capital} contain area of 
-            ${area} and a population is ${population}`);   
-    } 
+// Constructor function for creating country objects
+function country(countryname, area, population, capital) {
+    this.area = area; // Area property
+    this.countryname = countryname; // Country name property
+    this.population = population; // Population property
+    this.capital = capital; // Capital property
+    // Method to greet and provide information about the country
+    this.greeting = function() {
+        console.log(`Welcome to ${this.countryname}, which has a capital of ${this.capital}, an area of 
+            ${this.area} square kilometers, and a population of ${this.population}.`);
+    }
 }
+
+// Creating instances of the country constructor
 const monaco = new country("Monaco", 2.02, 39242, "Monaco");
 const finland = new country("Finland", 338424, 5540720, "Helsinki");
 const liechtenstein = new country("Liechtenstein", 160, 38896, "Vaduz");
 
-console.log(monaco.constructor);
-console.log(finland.constructor);
-console.log(liechtenstein.constructor);
+// Logging the constructor property of each country instance
+console.log(monaco.constructor); // Logs the constructor function for Monaco
+console.log(finland.constructor); // Logs the constructor function for Finland
+console.log(liechtenstein.constructor); // Logs the constructor function for Liechtenstein
 
-monaco.greeting();
-finland.greeting();
-liechtenstein.greeting();
+// Call the greeting method for each country instance
+monaco.greeting(); // Outputs information about Monaco
+finland.greeting(); // Outputs information about Finland
+liechtenstein.greeting(); // Outputs information about Liechtenstein
+
+
+// Output
+// When you run this code, the output will look like:
+
+// Name: Elon Musk
+// Gender: Male
+// Children: Nevada, Griffin, X Æ A-Xii, Exa Dark Sideræl
+// State: Texas
+// { name: 'Elon Musk', gender: 'Male', children: [ 'Nevada', 'Griffin', 'X Æ A-Xii', 'Exa Dark Sideræl' ], state: 'Texas', getInfo: [Function: getInfo] }
+// function country(countryname, area, population, capital) { ... }
+// function country(countryname, area, population, capital) { ... }
+// function country(countryname, area, population, capital) { ... }
+// Welcome to Monaco, which has a capital of Monaco, an area of 2.02 square kilometers, and a population of 39242.
+// Welcome to Finland, which has a capital of Helsinki, an area of 338424 square kilometers, and a population of 5540720.
+// Welcome to Liechtenstein, which has a capital of Vaduz, an area of 160 square kilometers, and a population of 38896.
