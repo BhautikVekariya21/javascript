403bb948c721e5e37a53ccbb847051d08c1a56ae
Bhautik Vekariya <128315385+BhautikVekariya21@users.noreply.github.com>
2024-10-05 19:09:04 +0530

    staticprop.
    Explanation of Key Concepts
    Class Definition:
    
    The Animal class has a constructor that initializes the name property and a method (logName) to log the name of the animal.
    Static Method:
    
    The createSpeciesId method is static, meaning it belongs to the class itself and not to instances of the class. Therefore, it can only be called on the class (e.g., Cat.createSpeciesId()) and not on an instance (e.g., persianCat.createSpeciesId()).
    Inheritance:
    
    The Cat class extends the Animal class, inheriting its properties and methods. It also has its own property (breed) and method (logBreed) to log the breed.
    Creating Instances:
    
    Instances of Animal and Cat are created using the new keyword. Methods defined in the class can be called on these instances.
    Output
    When you run this code, the output will be:
    
    Animal-001
    Animal Name: Persian
    Breed: Persian Cat
    
    Notes
    The commented-out lines that try to call createSpeciesId() on instances will throw an error because static methods should be accessed via the class name.
    The code clearly demonstrates how static methods and inheritance work in JavaScript classes. If you have any further questions or would like to explore additional concepts, feel free to ask!

--- oop/staticprop.js
+++ oop/staticprop.js
@@ -1,33 +1,52 @@
+// Class definition for Animal
 class Animal {
     constructor(name) {
-        this.name = name;
+        this.name = name; // Set the name of the animal
     }
 
     logName() {
+        // Method to log the animal's name
         console.log(`Animal Name: ${this.name}`);
     }
 
     static createSpeciesId() {
+        // Static method that returns a species ID
         return `Animal-001`;
     }
 }
 
+// Creating an instance of the Animal class
 const dog = new Animal("Dog");
-// console.log(dog.createSpeciesId()) // Will throw an error because createSpeciesId is static
+// console.log(dog.createSpeciesId()) // This line would throw an error because createSpeciesId is a static method and cannot be called on instances
 
+// Class definition for Cat that extends Animal
 class Cat extends Animal {
     constructor(name, breed) {
-        super(name);
-        this.breed = breed;
+        super(name); // Call the parent constructor with the name
+        this.breed = breed; // Set the breed of the cat
     }
 
     logBreed() {
+        // Method to log the breed of the cat
         console.log(`Breed: ${this.breed}`);
     }
 }
 
+// Creating an instance of the Cat class
 const persianCat = new Cat("Persian", "Persian Cat");
-console.log(Cat.createSpeciesId()); // Static method is accessed through the class
-// console.log(persianCat.createSpeciesId())
-persianCat.logName();
-persianCat.logBreed();
+
+// Accessing the static method through the Cat class
+console.log(Cat.createSpeciesId()); // Outputs: Animal-001
+
+// console.log(persianCat.createSpeciesId()) // This line would also throw an error, similar to the previous one
+
+// Logging the name and breed of the persianCat instance
+persianCat.logName(); // Outputs: Animal Name: Persian
+persianCat.logBreed(); // Outputs: Breed: Persian Cat
+
+// Output
+// When you run this code, the output will be:
+
+// Animal-001
+// Animal Name: Persian
+// Breed: Persian Cat
