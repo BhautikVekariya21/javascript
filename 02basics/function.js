7614a1878afa30b083e0e16b84ca8ab5aae12f64
Bhautik Vekariya <128315385+BhautikVekariya21@users.noreply.github.com>
2024-10-05 16:35:37 +0530

    callback function

--- 02basics/function.js
+++ 02basics/function.js
@@ -1,49 +1,64 @@
-function greet (){
+// A function with a greeting message (currently commented out)
+function greet() {
     // console.log("good morning to the cutiepie" + " looking very nice");
 }
 
+// Calling the 'greet' function (nothing will happen because it's commented out)
 greet();
 
-function summetion (number1,number2){
-    let result = number1+number2;
+// Function to sum two numbers and return the result
+function summetion(number1, number2) {
+    let result = number1 + number2;
     return result;
 }
 
-const summ = summetion(11,21);
-// console.log(summ);
+// Calling the summetion function and storing the result in 'summ'
+const summ = summetion(11, 21);
+// console.log(summ); // Output: 32
 
-function usermessage(username="Dhruv"){
-    return `${username} you are arrived`
+// Function with a default parameter (username) to return a message
+function usermessage(username = "Dhruv") {
+    return `${username} you are arrived`;
 }
-// console.log(usermessage("Brinjal"))
+// console.log(usermessage("Brinjal")); // Output: "Brinjal you are arrived"
 
-function totalprice(value1,value2,value3,...num){
-    return num
+// Function using rest parameters to collect additional arguments
+function totalprice(value1, value2, value3, ...num) {
+    return num; // Returning the collected arguments as an array
 }
-// console.log(totalprice(121,11,117,112,254,2112,212));
+// console.log(totalprice(121, 11, 117, 112, 254, 2112, 212)); // Output: [112, 254, 2112, 212]
 
+// Using a Symbol as a unique property in an object
 const smb = Symbol("8923245");
 const vocal = {
-    name :"carrot",
-    "scientific name":"Daucus carota",
-    location:"china",
-    [smb]:"boiling",
-    price:90,
-    email:"carrot@carrot.com",
-    containnow:false,
-    majorproducer:["china","russia","us"],
-    season:"winter"
-}
+    name: "carrot",
+    "scientific name": "Daucus carota",
+    location: "china",
+    [smb]: "boiling", // Symbol property, which won't appear in regular loops
+    price: 90,
+    email: "carrot@carrot.com",
+    containnow: false,
+    majorproducer: ["china", "russia", "us"], // Array of producers
+    season: "winter"
+};
 
-function localhost (anyobject){
-    console.log(`name is ${anyobject.name} and location is ${anyobject.location} and major producer id ${anyobject.majorproducer}`);
+// Function to log some properties of an object
+function localhost(anyobject) {
+    console.log(`name is ${anyobject.name} and location is ${anyobject.location} and major producer is ${anyobject.majorproducer}`);
 }
-localhost(vocal);
+// Calling the 'localhost' function with the 'vocal' object
+localhost(vocal); 
+// Output: "name is carrot and location is china and major producer is china,russia,us"
 
-const numstan =[99,89,97,112]
+// Array of numbers
+const numstan = [99, 89, 97, 112];
 
-function numbr(anyarray){
-    console.log(anyarray[2]);
+// Function to log the third element of an array
+function numbr(anyarray) {
+    console.log(anyarray[2]); // Accessing the element at index 2
 }
-console.log(numbr(numstan));
-console.log(numbr([11,23,41,31]))
+// Calling 'numbr' with the 'numstan' array
+console.log(numbr(numstan)); // Output: 97
+
+// Calling 'numbr' with a different array
+console.log(numbr([11, 23, 41, 31])); // Output: 41
