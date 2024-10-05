2334b10ac250a99c0ca1cfd90bc2bc5d10f552f0
Bhautik Vekariya <128315385+BhautikVekariya21@users.noreply.github.com>
2024-10-05 16:51:51 +0530

    Destructring a
    Dynamic Object Creation: Properties can be added to objects dynamically at any point.
    Nested Objects: You can create and access deeply nested objects, which is common in structured data.
    Object Merging: You can merge multiple objects using Object.assign() or the spread operator.
    Arrays of Objects: JavaScript allows you to create arrays of objects, providing flexibility in data handling.
    Property Checks: hasOwnProperty() can be used to check for the existence of specific properties.
    Symbols: Using Symbol as property keys ensures uniqueness and prevents naming conflicts.
    Destructuring: This provides a clean way to extract multiple properties from objects and rename them as needed.

--- array/02object.js
+++ array/02object.js
@@ -1,87 +1,94 @@
-const zucchini = {}
-zucchini.id="zucch11"
-zucchini.name="yellow zucchinni"
-zucchini.contain=true
+// Creating an empty object 'zucchini' and dynamically adding properties
+const zucchini = {};
+zucchini.id = "zucch11";            // Adding an ID for the zucchini
+zucchini.name = "yellow zucchinni"; // Adding a name for the zucchini
+zucchini.contain = true;            // Adding a boolean property indicating if it contains something (e.g., seeds)
 
+// Uncomment to log the 'zucchini' object to the console
 // console.log(zucchini);
 
+// Creating a nested object 'kale' with an email and a full name
 const kale = {
     email: "kale@kale.com",
     fullname: {
         userfullname: {
-            firstname: "kale",
-            lastname: "Brassica oleracea var. sabellica"
+            firstname: "kale",  // First name property
+            lastname: "Brassica oleracea var. sabellica" // Last name (scientific name)
         }
     }
-}
+};
+// Uncomment to log the last name of the kale object
 // console.log(kale.fullname.userfullname.lastname);
 
-const veg1= {1:"Artichoke",2:"Arugula",3:"Asparagus",4:"Bell Peppers"}
+// Creating several vegetable objects with numeric keys to represent their names
+const veg1 = {1: "Artichoke", 2: "Arugula", 3: "Asparagus", 4: "Bell Peppers"};
+const veg2 = {5: "Beet Greens", 6: "Beets", 7: "Bok Choy", 8: "Broccoli"};
+const veg3 = {9: "Broccoli Rabe", 10: "Brussels Sprouts", 11: "Butternut Squash", 12: "Cabbage (Green)"};
+const veg4 = {13: "Cabbage", 14: "Carrots", 15: "Cauliflower", 16: "Celery"};
+const veg5 = {17: "Chives", 18: "Collard Greens", 19: "Crookneck Squash", 20: "Daikon Radish"};
+const veg6 = {21: "Dandelion Greens", 22: "Eggplant", 23: "Endive", 24: "Jicama"};
+const veg7 = {25: "Green Onion", 26: "Kohlrabi", 27: "Shallots", 28: "Spinach"};
 
-const veg2= {5:"Beet Greens",6:"Beets",7:"Bok Choy",8:"Broccoli"}
+// Merging veg1 and veg2 into a new object using an object literal
+const veg8 = { veg1, veg2 }; // veg8 becomes an object with veg1 and veg2 as properties
 
-const veg3= {9:"Broccoli Rabe",10:"Brussels Sprouts",11:"Butternut Squash",12:"Cabbage (Green)"}
+// Merging multiple vegetable objects into one using Object.assign
+const veg9 = Object.assign({}, veg1, veg2, veg4); // Combines veg1, veg2, and veg4 into a new object
 
-const veg4= {13:"Cabbage",14:"Carrots",15:"Cauliflower",16:"Celery"}
+// Using the spread operator to merge veg1 and veg2 into a new object
+const veg10 = { ...veg1, ...veg2 }; // Creates a new object with properties from both veg1 and veg2
 
-const veg5= {17:"Chives",18:"Collard Greens",19:"Crookneck Squash",20:"Daikon Radish"}
-
-const veg6= {21:"Dandelion Greens",22:"Eggplant ",23:"Endive",24:"Jicama"}
-
-const veg7= {25:"Green Onion",26:"Kohlrabi",27:"Shallots",28:"Spinach"}
-
-const veg8 = { veg1, veg2 }
-const veg9 = Object.assign({}, veg1, veg2, veg4)
-
-// console.log(veg8);
-// console.log(veg9);
-const veg10 ={...veg1,...veg2 }
-
-const user =[
+// Creating an array of user objects with unique keys and names
+const user = [
     {
-        ket:11,
-        name:'Tomatoes'
+        ket: 11,            // Each user object has a 'ket' property
+        name: 'Tomatoes'    // and a 'name' property
     },
     {
-        ket:21,
-        name:"Turnips"
+        ket: 21,
+        name: "Turnips"
     },
     {
-        ket:41,
-        name:'Spinach'
+        ket: 41,
+        name: 'Spinach'
     },
-]
-console.log(user);
-console.log(user[1].name);
-console.log(Object.keys(user));
-console.log(Object.values(user));
-console.log(Object.entries(user));
+];
 
-console.log(zucchini.hasOwnProperty('conatain'));
+// Logging the user array and accessing specific properties
+console.log(user);                          // Logs the entire user array
+console.log(user[1].name);                  // Accessing the name of the second user (Turnips)
+console.log(Object.keys(user));              // Logs the keys of the user array (the array indices)
+console.log(Object.values(user));            // Logs the values of the user array (the user objects)
+console.log(Object.entries(user));           // Logs key-value pairs of the user array (each user's index and object)
 
+// Checking if the 'zucchini' object has a property 'conatain' (note the typo)
+console.log(zucchini.hasOwnProperty('conatain')); // Outputs: false, as the property does not exist
 
+// Creating a unique symbol for a property
 const smb = Symbol("8923245");
 
+// Defining a more complex object 'vocal' with various properties
 const vocal = {
-    name :"carrot",
-    "scientific name":"Daucus carota",
-    location:"china",
-    [smb]:"boiling",
-    price:90,
-    email:"carrot@carrot.com",
-    containnow:false,
-    majorproducer:["china","russia","us"],
-    season:"winter"
-}
+    name: "carrot",                                           // Common name
+    "scientific name": "Daucus carota",                     // Scientific name with a space in the key
+    location: "china",                                       // Location of the carrot
+    [smb]: "boiling",                                        // Using a symbol as a property key
+    price: 90,                                               // Price property
+    email: "carrot@carrot.com",                              // Email property
+    containnow: false,                                       // Boolean property indicating some state
+    majorproducer: ["china", "russia", "us"],               // Array of major producers
+    season: "winter"                                         // Season for harvesting
+};
 
-const {majorproducer} = vocal;
-console.log(majorproducer);
+// Using destructuring to extract properties from the 'vocal' object
+const { majorproducer } = vocal;                            // Extracting the major producer array
+console.log(majorproducer);                                  // Outputs: ["china", "russia", "us"]
 
-const {majorproducer:largestproducer} = vocal;
-console.log(largestproducer);
+const { majorproducer: largestproducer } = vocal;           // Renaming during destructuring
+console.log(largestproducer);                                // Outputs: ["china", "russia", "us"]
 
-const {location:loaclity} = vocal;
-console.log(loaclity);
+const { location: loaclity } = vocal;                       // Renaming 'location' to 'loaclity'
+console.log(loaclity);                                      // Outputs: "china"
 
-const {season:seasonal} = vocal;
-console.log(seasonal);
+const { season: seasonal } = vocal;                         // Renaming 'season' to 'seasonal'
+console.log(seasonal);                                      // Outputs: "winter"
